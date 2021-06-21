b5b8211a3f44f682cd143b49f35866fc0ecbf105
Jose Ordoñez <josemaord@gmail.com>
2021-06-20 22:44:38 -0600

    Add data annotations to DB entity

--- Movies.Core/Entities/Movie.cs
+++ Movies.Core/Entities/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,8 @@ namespace Movies.Core.Entities
 {
     public class Movie
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Title { get; set; }
         public int Year { get; set; }
