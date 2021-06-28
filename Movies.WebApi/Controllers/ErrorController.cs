4ce2377d27b7d137e14bb036dcf8bf93a204c849
Jose Ordoñez <josemaord@gmail.com>
2021-06-27 19:52:05 -0600

    Add Error controller

---
+++ Movies.WebApi/Controllers/ErrorController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movies.WebApi.Controllers
+{
+    [ApiExplorerSettings(IgnoreApi = true)]
+    [ApiController]
+    public class ErrorController : ControllerBase
+    {
+        [Route("/error")]
+        [HttpGet]
+        public IActionResult Error() => Problem();
+    }
+}
